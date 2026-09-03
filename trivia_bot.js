f06651cc88b7de78ce7c2bafb28c247117daccf7
Paul Promise <paul.promise@outlook.com>
2026-09-03 14:29:24 +0000

    completed trivail and bot greeting

---
+++ trivia_bot.js
@@ -0,0 +1,20 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName = "botname";
+let botLocation = "botlocation";
+let favoriteLanguage = "python";
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+let codingFact = favoriteLanguage + " is easy to learn";
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " It wasn't named after the snake.";
+
+console.log(codingFact);
+codingFact = favoriteLanguage + " t is older than Java";
+console.log(codingFact);
+
+console.log(
+  "It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".",
+);
